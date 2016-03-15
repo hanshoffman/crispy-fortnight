456d13b20f4562dd69c05b1fe1a1d4ae22b42c69
Hans Hoffman <hdot@GreenCouch.fios-router.home>
2016-03-14 21:45:02 -0400

    windows template methods coded

--- crispy/os_types/windows.py
+++ crispy/os_types/windows.py
@@ -1,2 +1,26 @@
-def test():
-    return "testing"
+from crispy.os_types.base import Base
+
+class Windows(Base):
+
+    def enum_os(self):
+        return Base.enum_os(self)
+
+
+    def enum_users(self):
+        return Base.enum_users(self)
+
+
+    def enum_applications(self):
+        return Base.enum_applications(self)
+
+
+    def enum_drives(self):
+        return Base.enum_drives(self)
+
+
+    def enum_printers(self):
+        return Base.enum_printers(self)
+
+
+    def get_ssh_keys(self):
+        return Base.get_ssh_keys(self)
