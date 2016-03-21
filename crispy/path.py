870e9a5dc44a466b43cc9cc584322a240da7e7c3
Hans Hoffman <hdot@GreenCouch.fios-router.home>
2016-03-20 22:27:16 -0400

    removed

--- crispy/path.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-thisdir = os.path.dirname(__file__)
-libdir = os.path.join(thisdir, '/Users/hdot/Documents/Code/crispy-fortnight/')
-
-if libdir not in sys.path:
-    sys.path.insert(0, libdir)
