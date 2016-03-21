5ca8f532b2df5f0f62b184daf9b0e6fe68d2b76f
Hans Hoffman <hdot@GreenCouch.fios-router.home>
2016-03-20 22:26:11 -0400

    started aes

---
+++ crispy/crypto/aes.py
@@ -0,0 +1,9 @@
+class AESCipher:
+    def __init__(self, key):
+        self.key = key
+        
+    def encrypt(self, plaintext):
+        pass
+    
+    def decrypt(self, ciphertext):
+        pass
