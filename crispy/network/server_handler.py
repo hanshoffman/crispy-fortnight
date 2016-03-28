840bb9044a28d95d9cf6a98a137b0087a6d11bcb
Hans Hoffman <hdot@GreenCouch.local>
2016-03-28 00:19:43 -0400

    logging across modules working… but not showing each module individually.

--- crispy/network/server_handler.py
+++ crispy/network/server_handler.py
@@ -1,24 +1,23 @@
 import logging
 import SocketServer
 
-module_logger = logging.getLogger('crispy_srv.CrispyTCPServerHandler')
+#module_logger = logging.getLogger('crispy_srv.SocketServer')
 
 class CrispyTCPServerHandler(SocketServer.BaseRequestHandler):
     def __init__(self, request, client_address, server):
         SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
-	self.logger = logging.getLogger('crispy_srv.CrispyTCPServerHandler')
 	return
     
     def setup(self):
-        self.debug("BaseRequestHandler: setup()")
+        logging.debug("BaseRequestHandler: setup()")
         return SocketServer.BaseRequestHandler.setup(self)
     
     def handle(self):
-        self.logger.debug("BaseRequestHandler: connection from {}".format(self.client_address))
+	logging.debug("BaseRequestHandler: connection from {}".format(self.client_address))
         # "Session {} opened ({}:{} <- {}:{})".format(self.current_id, server_ip, server_port, client_ip, client_port))
         self.request.sendall("I am your master")
         return
     
     def finish(self):
-        self.logger.debug("BaseRequestHandler: finish()")
+        logging.debug("BaseRequestHandler: finish()")
         return SocketServer.BaseRequestHandler.finish(self) 
--- server.py
+++ server.py
@@ -26,28 +26,30 @@ def main():
     host = config.get('NETWORK', 'host')
     port = config.getint('NETWORK', 'port')
 
-    logger = logging.getLogger('crispy_srv')
-    logger.setLevel(config.getint('LOGS', 'logLvl'))
+    #logger = logging.getLogger('crispy_srv')
+    #logger.setLevel(config.getint('LOGS', 'logLvl'))
     
-    fh = logging.FileHandler('crispy.log')
-    fh.setLevel(config.getint('LOGS', 'logLvl'))
+    #fh = logging.FileHandler('crispy.log')
+    #fh.setLevel(config.getint('LOGS', 'logLvl'))
     
-    console = logging.StreamHandler()
-    console.setLevel(logging.ERROR)
+    #console = logging.StreamHandler()
+    #console.setLevel(logging.ERROR)
     
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    console.setFormatter(formatter)
+    #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    #fh.setFormatter(formatter)
+    #console.setFormatter(formatter)
         
-    logger.addHandler(fh)
-    logger.addHandler(console)
+    #logger.addHandler(fh)
+    #logger.addHandler(console)
+
+    logging.basicConfig(filename='crispy.log', level=logging.DEBUG)
     
     try:
         server = CrispyTCPServer((host, port), CrispyTCPServerHandler)
-        logger.debug("Started server on {0}:{1}".format(server.server_address[0], server.server_address[1]))
+        logging.debug("Started server on {0}:{1}".format(server.server_address[0], server.server_address[1]))
         server.serve_forever()
     except KeyboardInterrupt:
-        logger.debug("Shutting down server")
+        logging.debug("Shutting down server")
         logging.shutdown()
         server.shutdown()
         server.socket.close()
