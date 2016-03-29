f4564c42fbc982ead3eaedcb7744126cd43407f0
Hans Hoffman <hdot@GreenCouch.local>
2016-03-28 20:03:08 -0400

    removed all except banner

--- crispy/constants.py
+++ crispy/constants.py
@@ -1,8 +1,3 @@
-from crispy.crypto.mime import Mime
-from crispy.os_types.macintosh import Mac
-from crispy.os_types.windows import Windows
-from crispy.os_types.debian import Debian
-
 BANNER = "                                                                     \n \
      ___           ___                       ___           ___                 \n \
     /  /\         /  /\        ___          /  /\         /  /\         ___    \n \
@@ -14,17 +9,4 @@ BANNER = "                                                                     \
  \  \:\  /:/   \  \::/~~~~      \__\::/  \  \::/ /:/   \  \::/       ~\~~\:\   \n \
   \  \:\/:/     \  \:\          /__/:/    \__\/ /:/     \  \:\         \  \:\  \n \
    \  \::/       \  \:\         \__\/       /__/:/       \  \:\         \__\/  \n \
-    \__\/         \__\/                     \__\/         \__\/                \n \
-                                                                 Version 1.1   \n"
-    
-BUFFER_SIZE = 1024
-
-CIPHER = Mime()
-
-EOF_STR = "EOF!EOF!"
-
-PLATFORMS = {
-             'Darwin' : Mac(),
-             'Windows': Windows(),
-             'Linux'  : Debian()
-             }
+    \__\/         \__\/                     \__\/         \__\/                \n"
