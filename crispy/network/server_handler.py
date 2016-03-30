be6371e1cb7707003ac20f44441b1ce6c9fa71cf
Hans Hoffman <hdot@GreenCouch.local>
2016-03-29 22:30:42 -0400

    cleaned up

--- crispy/network/server_handler.py
+++ crispy/network/server_handler.py
@@ -8,16 +8,7 @@ class CrispyTCPServerHandler(SocketServer.BaseRequestHandler):
         SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
 	return
     
-    def setup(self):
-        logger.debug("BaseRequestHandler: setup()")
-        return SocketServer.BaseRequestHandler.setup(self)
-    
     def handle(self):
 	logger.debug("BaseRequestHandler: connection from {}".format(self.client_address))
-        # "Session {} opened ({}:{} <- {}:{})".format(self.current_id, server_ip, server_port, client_ip, client_port))
-        self.request.sendall("I am your master")
+	self.request.sendall("I am your master")
         return
-    
-    def finish(self):
-        logger.debug("BaseRequestHandler: finish()")
-        return SocketServer.BaseRequestHandler.finish(self) 
