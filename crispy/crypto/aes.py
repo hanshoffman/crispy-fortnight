e4ff1ac58cbb4252fec2ecbd54167161e784bcda
Hans Hoffman <hdot@GreenCouch.local>
2016-03-29 22:27:42 -0400

    adding logging

--- crispy/crypto/aes.py
+++ crispy/crypto/aes.py
@@ -1,19 +1,29 @@
 import base64
+import logging
+
 from Crypto.Cipher import AES
 from Crypto import Random
 
+logger = logging.getLogger(__name__)
+
 class AESCipher:
+    """ Simple AES encrypt/decrypt. """
+
     def __init__(self, key):
         self.key = key
         
     def encrypt(self, plaintext):
+	logger.debug("AES encrypt() called")
 	plaintext = pad(plaintext)
 	iv = Random.new().read(AES.block_size)
 	cipher = AES.new(self.key, AES.MODE_CBC, iv)
+
 	return base64.b64encode(iv + cipher.encrypt(plaintext))
     
     def decrypt(self, ciphertext):
+	logger.debug("AES decrypt called")
 	enc = base64.b64decode(ciphertext)
 	iv = en[:16]
 	cipher = AES.new(self.key, AES.MODE_CBC, iv)
+
 	return unpad(cipher.decrypt(ciphertext[16:]))
