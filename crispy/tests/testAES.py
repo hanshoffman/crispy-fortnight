3872f907b5f74da9aad505dd7c9832978c45885a
Hans Hoffman <hdot@GreenCouch.local>
2016-03-29 22:26:51 -0400

    missing Crypto module… need to use pip to install

---
+++ crispy/tests/testAES.py
@@ -0,0 +1,27 @@
+import unittest
+
+from .. crypto.aes import AES
+
+class AESTestCases(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.cipher = AES()
+
+    def testEncode1(self):
+        """ Test Encode 1 """
+        self.assertEqual(self.cipher.encode('crispy'), 'Y3Jpc3B5')
+
+    def testEncode2(self):
+        """ Test Encode 2 """
+        self.assertEqual(self.cipher.encode('crispy123!#$% asdfn .. ss'), 'Y3Jpc3B5MTIzISMkJSBhc2RmbiAuLiBzcw==')
+
+    def testDecode1(self):
+        """ Test Decode 1 """
+        self.assertEqual(self.cipher.decode("dGhpcyBpcyBhIHRlc3Q="), "this is a test")
+
+    def testDecode2(self):
+        """ Test Decode 2 """
+        self.assertNotEqual(self.cipher.decode("YWdlbnQ="), "second")
+
+if __name__ == '__main__':
+    unittest.main()
