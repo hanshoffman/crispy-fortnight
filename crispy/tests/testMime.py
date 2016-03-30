9f7fa62e582ff26b8b6f5eb68c008700ec7d01a4
Hans Hoffman <hdot@GreenCouch.local>
2016-03-29 22:26:21 -0400

    update

--- crispy/tests/testMime.py
+++ crispy/tests/testMime.py
@@ -1,6 +1,6 @@
 import unittest
 
-from crispy.crypto.mime import Mime
+from .. crypto.mime import Mime
 
 class MimeTestCases(unittest.TestCase):
     @classmethod
@@ -8,19 +8,19 @@ class MimeTestCases(unittest.TestCase):
         cls.cipher = Mime()
         
     def testEncode1(self):
-        """Test Encode 1"""
+        """ Test Encode 1 """
         self.assertEqual(self.cipher.encode('crispy'), 'Y3Jpc3B5')
         
     def testEncode2(self):
-        """Test Encode 2"""
+        """ Test Encode 2 """
         self.assertEqual(self.cipher.encode('crispy123!#$% asdfn .. ss'), 'Y3Jpc3B5MTIzISMkJSBhc2RmbiAuLiBzcw==')
 
     def testDecode1(self):
-        """Test Decode 1"""
+        """ Test Decode 1 """
         self.assertEqual(self.cipher.decode("dGhpcyBpcyBhIHRlc3Q="), "this is a test")
         
     def testDecode2(self):
-        """Test Decode 2"""
+        """ Test Decode 2 """
         self.assertNotEqual(self.cipher.decode("YWdlbnQ="), "second")
     
 if __name__ == '__main__':
