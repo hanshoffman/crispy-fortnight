cb4afc124412db47c27cfcf476fb8b7e6f7b6c98
Hans Hoffman <hdot@GreenCouch.local>
2016-03-29 22:29:53 -0400

    added logging

--- crispy/lib/server.py
+++ crispy/lib/server.py
@@ -1,8 +1,14 @@
+import logging
 import SocketServer
 
-from crispy.network.server_handler import CrispyTCPServerHandler
+from .. network.server_handler import CrispyTCPServerHandler
+from . connection import CrispyConnection
+
+logger = logging.getLogger(__name__)
 
 class CrispyTCPServer(SocketServer.TCPServer):
+    """ Backend server methods for clients. """
+
     def __init__(self, server_address, handler_class=CrispyTCPServerHandler):
         self.allow_reuse_address = True
         self.clients = []
@@ -11,13 +17,18 @@ class CrispyTCPServer(SocketServer.TCPServer):
         return
 
     def add_client(self, conn):
-        """Add remote implant to CrispyServer's sessions list"""
-        self.clients.append(conn)
+        """ Add remote implant to clients list. """
+	logger.debug("add_client() was called")
+	#new_client = CrispyConnection({})
+        # "Session {} opened ({}:{} <- {}:{})".format(self.current_id, server_ip, server_port, client_ip, client_port))
+	self.clients.append(new_client)
     
     def remove_client(self, client):
-        """Remove remote implant from CrispyServer's sessions list"""
+        """ Remove remote implant from clients list. """
+	logger.debug("remove_client() was called")
         self.clients.remove(client)
     
-    def get_client_list(self):
-        """Return a list of sessions connected to the CrispyServer"""
+    def list_clients(self):
+        """ Return a list of sessions connected to the C2 server. """
+	logger.debug("list_clients() was called")
         return self.clients
