6c39e225b7a00fb9b17de706a851e42003b69a59
Hans Hoffman <hdot@GreenCouch.local>
2016-03-29 22:28:36 -0400

    added debugging

--- crispy/lib/connection.py
+++ crispy/lib/connection.py
@@ -1,55 +1,67 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 class CrispyConnection(object):
     def __init__(self, desc):
         self.desc = desc
         self.conn = self.desc["conn"]
     
     def __str__(self):
-        """Return string representing a CrispyConnection object (full)."""
-        return "Connection(id=%d, user=%s, platform=%s, hostname=%s, macaddr=%s)" %(self.desc["id"], self.desc["user"], self.desc["platform"], self.desc["hostname"], self.desc["macaddr"])
+        """ Return string representing a CrispyConnection object (full). """
+        logger.debug("__str__() was called")
+	return "Connection(id=%d, user=%s, platform=%s, hostname=%s, macaddr=%s)" %(self.desc["id"], self.desc["user"], self.desc["platform"], self.desc["hostname"], self.desc["macaddr"])
     
     def short_name(self):
-	"""Return string representing a CrispyConnection object (short)."""
+	""" Return string representing a CrispyConnection object (short). """
+	logger.debug("short_name() was called")
 	return self.desc["platform"][:3].lower() + "_" + self.desc["hostname"] + "_" + self.desc["macaddr"] 
 
     def is_android(self):
-        """Determine if platform connected is an Android device."""
+        """ Determine if platform connected is an Android device. """
+	logger.debug("is_android() was called")
         if "Android" in self.desc["platform"]:
             return True
         else:
             return False
     
     def is_darwin(self):
-        """Determine if platform connected is a Macintosh system."""
-        if "Darwin" in self.desc["platform"]:
+        """ Determine if platform connected is a Macintosh system. """
+        logger.debug("is_darwin() was called")
+	if "Darwin" in self.desc["platform"]:
             return True
         else:
             return False
     
     def is_linux(self):
-        """Determine if platform connected is an Linux system."""
-        if "Linux" in self.desc["platform"]:
+        """ Determine if platform connected is an Linux system. """
+        logger.debug("is_linux was called")
+	if "Linux" in self.desc["platform"]:
             return True
         else:
             return False
     
     def is_windows(self):
-        """Determine if platform connected is a Windows system."""
-        if "Windows" in self.desc["platform"]:
+        """ Determine if platform connected is a Windows system. """
+        logger.debug("is_windows() was called")
+	if "Windows" in self.desc["platform"]:
             return True
         else:
             return False
 
     def is_unix(self):
-	"""Determine if platform connected is a *nix system."""
+	""" Determine if platform connected is a *nix system. """
 	return not self.is_windows()
     
     def is_proc_arch_64_bits(self):
-        """Determine if platform connected is a 64-bit architecture"""
-        if "64" in self.desc["proc_arch"]:
+        """ Determine if platform connected is a 64-bit architecture. """
+        logger.debug("is_proc_arch_64_bits() was called")
+	if "64" in self.desc["proc_arch"]:
             return True
         else:
             return False
 
     def run_module(self, module_name, args):
-	"""Start a module on client"""
+	""" Start a module on client. """
+	logger.debug("run_module() was called")
 	pass
