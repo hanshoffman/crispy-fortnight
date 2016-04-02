c02432964214358d2a33b107a2ecffdf7ecc0fa0
Hans Hoffman <hdot@GreenCouch.fios-router.home>
2016-04-02 16:14:24 -0400

    renamed to a shorter name

--- crispy/network/handler.py
+++ crispy/network/handler.py
@@ -1,6 +1,9 @@
 import logging
 import SocketServer
 
+from .. lib.connection import CrispyConnection
+from .. network.client_types import CrispyTCPClient
+
 logger = logging.getLogger(__name__)
 
 class CrispyTCPServerHandler(SocketServer.BaseRequestHandler):
@@ -9,5 +12,7 @@ class CrispyTCPServerHandler(SocketServer.BaseRequestHandler):
 	return
     
     def handle(self):
-	logger.debug("connection from {}".format(self.client_address[0]))
-        return
+	conn = CrispyConnection({"conn":self, "id":1, "user":"hdot", "platform":"darwin", "hostname":"GreenCouch.local", "macaddr":"60:f8:1d:b7:8e:b2"})
+	self.server.add_session(conn)
+	logger.debug(conn)
+	return
