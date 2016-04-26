91b07fb06849438e85d86b6e618ab7bb07509c79
Hans Hoffman <hdot@GreenCouch.fios-router.home>
2016-04-25 21:55:17 -0400

    custom print library

---
+++ crispy/lib/fprint.py
@@ -0,0 +1,18 @@
+colors = {"red":"\033[0;31;40m", "green":"\033[0;32;40m", "yellow":"\033[0;33;40m", "gray":"\033[0;37;40m"}
+color_stop = "\033[0m"
+
+def error(msg):
+    """ Return a formatted error line to stdout. """
+    print "{}[!] {}{}".format(colors["red"], msg, color_stop)
+
+def info(msg):
+    """ Return a formatted info line to stdout. """
+    print "{}[*] {}{}".format(colors["gray"], msg, color_stop)
+
+def success(msg):
+    """ Return a formatted success line to stdout. """
+    print "{}[+] {}{}".format(colors["green"], msg, color_stop)
+
+def warning(msg):
+    """ Return a formatted warning line to stdout. """
+    print "{}[-] {}{}".format(colors["yellow"], msg, color_stop)
