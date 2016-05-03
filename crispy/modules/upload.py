61036677a05d6ca6b34849ce116152fcb70f200c
Hans Hoffman <hdot@GreenCouch.fios-router.home>
2016-05-02 23:45:21 -0400

    finished

--- crispy/modules/upload.py
+++ crispy/modules/upload.py
@@ -15,19 +15,19 @@ class UploadModule(CrispyModule):
 
     def check_args(self, args):
         self.parser = CrispyArgumentParser(prog="upload", description=self.__doc__)
-        self.parser.add_argument("local_file", metavar="<local_path>", type=str)
-        self.parser.add_argument("remote_file", metavar="<remote_path>", nargs="?", type=str)
+        self.parser.add_argument("local_file", metavar="<local_file_path>", type=str)
+        self.parser.add_argument("remote_file", metavar="<remote_file_path>", type=str)
    
         return self.parser.parse_args(args)
 
     def run(self, args):
         logger.debug("run(args) was called.")
-        info("Attempting upload of {}...".format(args.local_file))
-        
-        try: 
+        info("Attempting to upload file...")
+       
+        try:
             upload(self.client.conn, args.local_file, args.remote_file)
             success("File transfer complete.")
             logger.info("File transfer complete.")
         except Exception as e:
-            error("File transfer failed.")
+            error("File transfer failed: {}".format(e))
             logger.error("File transfer failed.")
