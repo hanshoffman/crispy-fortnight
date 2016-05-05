6e30e5171a9a6adad3f92b5283f3c982217d4e50
Hans Hoffman <hdot@GreenCouch.fios-router.home>
2016-05-04 23:49:22 -0400

    formatting

--- crispy/modules/users.py
+++ crispy/modules/users.py
@@ -20,10 +20,17 @@ class UsersModule(CrispyModule):
         #http://stackoverflow.com/questions/421618/python-script-to-list-users-and-groups
         #format like drives.py
         if (self.is_compatible()):
-            print "\nUsers:\n==================="
+            spacing = "%-8s %15s %8s %8s %12s %12s"
+            print spacing % (("\nName", "Full Name", "UID", "GID", "Home", "Shell"))
+            
             try:
                 if self.client.is_darwin():
                     users = self.client.conn.modules['os'].listdir('/Users')
+                    for user in self.client.conn.modules['pwd'].getpwall():
+                        #defautl mac osx gid is 20 or "staff" for non-system users
+                        if user[2] == 0 or (user[2] > 500 and user[3] == 20):
+                            print spacing % (user[0], user[4], user[2], user[3], user[5], user[6])
+                            #create a dictionary to keep unique values then print at end to avoid duplicates?
                 elif self.client.is_unix():
                     for user in self.client.conn.modules['pwd'].getpwall():
                         #useradd man pages claims that users w/ gid 0-999 are typically reserved for system accounts
