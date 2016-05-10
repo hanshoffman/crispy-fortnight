fdb37cb24d90cc716e6cc0828c46550bbe56c66d
WesleyThurner <wesleythurner@gmail.com>
2016-05-10 09:46:39 -0400

    preliminary unix apps

--- crispy/modules/apps.py
+++ crispy/modules/apps.py
@@ -12,6 +12,9 @@ class AppsModule(CrispyModule):
     # can be: 'Darwin', 'Linux', 'Windows', 'Android'
     compatible_systems = ['Darwin']
 
+    def unix_listpackages(self, package_managers):
+        attempt_unix_apps = self.client.conn.modules['subprocess'].Popen(package_managers, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
     def run(self, args):
         logger.debug("run(args) was called")
 
@@ -20,14 +23,21 @@ class AppsModule(CrispyModule):
 
             try:
                 if self.client.is_darwin():
-                    apps = self.client.conn.modules['os'].listdir('/Applications') 
+                    apps = self.client.conn.modules['os'].listdir('/Applications')
                     for app in apps:
-                        if app.endswith(".app"): 
+                        if app.endswith(".app"):
                             try:
                                 pl = self.client.conn.modules['plistlib'].readPlist('/Applications/' + app + '/Contents/Info.plist')
                                 print app[:-4] + " " + pl["CFBundleShortVersionString"]
                             except:
                                 print app[:-4] + " [No version in plist]"
+                elif self.client.is_unix():
+                    package_managers = ['dpkg --get-selections', 'yum list installed', 'echo test 3']
+                    for a in package_managers:
+                        try:
+                            attempt_unix_apps(a)
+                        except OSError:
+                            continue
             except Exception as e:
                 logger.error(e)
                 error(e)
