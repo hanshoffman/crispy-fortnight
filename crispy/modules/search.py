df472eda8d021dd64fef86b0205a3b9a017c9949
Hans Hoffman <hdot@GreenCouch.fios-router.home>
2016-05-19 15:52:30 -0400

    added basic functionality

--- crispy/modules/search.py
+++ crispy/modules/search.py
@@ -1,4 +1,3 @@
-import cPickle
 import logging
 
 from crispy.lib.myparser import CrispyArgumentParser
@@ -11,11 +10,28 @@ __class_name__ = "SearchModule"
 class SearchModule(CrispyModule):
     """ Search for a file/files on a remote machine. """
 
-    compatible_systems = ['all']
+    compatible_systems = ['Darwin']
     
-    def init_argparse(self):
+    def check_args(self, args):
         self.parser = CrispyArgumentParser(prog="search", description=self.__doc__)
-        #self.parser.add_argument()
+        self.parser.add_argument("--path", metavar="<path>", help="starting path", required=True, type=str)
+        self.parser.add_argument("--ext", metavar="<extension>", help="ie. pdf, txt, zip", required=True, type=str)
+        self.parser.add_argument("search_blob", metavar="<search_blob>", type=str)
+
+        return self.parser.parse_args(args)
 
     def run(self, args):
-        pass        
+        logger.debug("run(args) was called")
+        info("Searching for files...")
+
+        try:
+            for path, dirs, files in self.client.conn.modules['os'].walk(args.path):
+                for name in files:
+                    if args.search_blob: #have to include stuff like *name or *name*
+                        pass
+                    if name.endswith(args.ext):
+                        print "{}/{}".format(self.client.conn.modules['os'].path.realpath(path), name)
+            success("Done.")
+        except Exception as e:
+            error(e)
+            logger.error(e)
