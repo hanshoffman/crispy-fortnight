81a5fa71e4a82da4d0b2515bf809eca7e7851955
Hans Hoffman <hdot@GreenCouch.fios-router.home>
2016-05-19 16:44:00 -0400

    added keyword search

--- crispy/modules/search.py
+++ crispy/modules/search.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 from crispy.lib.myparser import CrispyArgumentParser
 from crispy.lib.module import *
@@ -16,7 +17,7 @@ class SearchModule(CrispyModule):
         self.parser = CrispyArgumentParser(prog="search", description=self.__doc__)
         self.parser.add_argument("--path", metavar="<path>", help="starting path", required=True, type=str)
         self.parser.add_argument("--ext", metavar="<extension>", help="ie. pdf, txt, zip", required=True, type=str)
-        self.parser.add_argument("search_blob", metavar="<search_blob>", type=str)
+        self.parser.add_argument("--keyword", metavar="<keyword>", type=str)
 
         return self.parser.parse_args(args)
 
@@ -25,12 +26,14 @@ class SearchModule(CrispyModule):
         info("Searching for files...")
 
         try:
-            for path, dirs, files in self.client.conn.modules['os'].walk(args.path):
-                for name in files:
-                    if args.search_blob: #have to include stuff like *name or *name*
-                        pass
+            for (dirpath, dirnames, filenames) in self.client.conn.modules['os'].walk(args.path):
+                for name in filenames:
                     if name.endswith(args.ext):
-                        print "{}/{}".format(self.client.conn.modules['os'].path.realpath(path), name)
+                        if args.keyword:
+                            if re.search(args.keyword, name):
+                                print "{}/{}".format(self.client.conn.modules['os'].path.realpath(dirpath), name)
+                        else:
+                            print "{}/{}".format(self.client.conn.modules['os'].path.realpath(dirpath), name)
             success("Done.")
         except Exception as e:
             error(e)
