79039b3cf061e7fe6c392537d99645ae3c931328
Hans Hoffman <hdot@GreenCouch.fios-router.home>
2016-05-19 17:41:42 -0400

    added group

--- crispy/modules/search.py
+++ crispy/modules/search.py
@@ -11,12 +11,12 @@ __class_name__ = "SearchModule"
 class SearchModule(CrispyModule):
     """ Search for a file/files on a remote machine. """
 
-    compatible_systems = ['Darwin']
+    compatible_systems = ['all']
     
     def check_args(self, args):
         self.parser = CrispyArgumentParser(prog="search", description=self.__doc__)
         self.parser.add_argument("--path", metavar="<path>", help="starting path", required=True, type=str)
-        self.parser.add_argument("--ext", metavar="<extension>", help="ie. pdf, txt, zip", required=True, type=str)
+        self.parser.add_argument("--ext", metavar="<extension>", help="ie. pdf, txt, zip", type=str)
         self.parser.add_argument("--keyword", metavar="<keyword>", type=str)
 
         return self.parser.parse_args(args)
@@ -26,14 +26,18 @@ class SearchModule(CrispyModule):
         info("Searching for files...")
 
         try:
-            for (dirpath, dirnames, filenames) in self.client.conn.modules['os'].walk(args.path):
-                for name in filenames:
-                    if name.endswith(args.ext):
-                        if args.keyword:
-                            if re.search(args.keyword, name):
-                                print "{}/{}".format(self.client.conn.modules['os'].path.realpath(dirpath), name)
-                        else:
-                            print "{}/{}".format(self.client.conn.modules['os'].path.realpath(dirpath), name)
+            if not args.ext and not args.keyword:
+                raise Exception("must have either --ext or --keyword or both")
+            else:
+                for (dirpath, dirnames, filenames) in self.client.conn.modules['os'].walk(args.path):
+                    for name in filenames:
+                        if args.ext: 
+                            if name.endswith(args.ext):
+                                if args.keyword:
+                                    if re.search(args.keyword, name):
+                                        print "{}/{}".format(self.client.conn.modules['os'].path.realpath(dirpath), name)
+                                else:
+                                    print "{}/{}".format(self.client.conn.modules['os'].path.realpath(dirpath), name)
             success("Done.")
         except Exception as e:
             error(e)
